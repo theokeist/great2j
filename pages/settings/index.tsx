d0e48476a5a28d7d245b32d1fbc638cc7ac0c697
Martin Orszulik <martin.orszulik@techfides.cz>
2022-02-08 16:37:22 +0100

    WARRNING, added hardcoded env

--- pages/settings/index.tsx
+++ pages/settings/index.tsx
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export default createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  "https://brtzsfbibxcwxjpqlptw.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MzYyNTYwOCwiZXhwIjoxOTU5MjAxNjA4fQ.aTGAfKBnHuGzNbJRFn7fGRLxzImWfpKbGUW3-SNv5mY"
 );
