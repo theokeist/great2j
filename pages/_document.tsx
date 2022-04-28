f47b9f53b2a652754abb04d076712d9bfd6afcba
Martin Orszulik <orszulik.m@outlook.com>
2022-04-28 11:17:48 +0200

    ADD SEO INIT

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,6 +15,7 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,700;1,900&display=swap"
             rel="stylesheet"
           />
+          <meta name="robots" content="all" />
         </Head>
         <body>
           <Main />
